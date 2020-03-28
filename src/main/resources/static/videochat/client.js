e05108fac637426a6d2ea2875250961efeee7cbc
surajraoiam <62713528+surajraoiam@users.noreply.github.com>
2020-03-28 18:46:03 +0530

    remove ssl in client.js

--- src/main/resources/static/videochat/client.js
+++ src/main/resources/static/videochat/client.js
@@ -26,7 +26,7 @@ var connectedUser;
 
 //connecting to our signaling server
 var loc = window.location;
-var conn = new WebSocket('wss://'+loc.hostname+'/socket');
+var conn = new WebSocket('ws://'+loc.hostname+'/socket');
   
 conn.onopen = function () { 
 	   console.log("Connected to the signaling server"); 
